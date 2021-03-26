5833cd344e54b02d3e98c2941c3b3d29f1d6d536
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-03-26 19:39:36 +0100

    Add plugin mongo.db.js

---
+++ auth/src/plugins/mongo.db.js
@@ -0,0 +1,23 @@
+const fp = require('fastify-plugin')
+const isDocker = require('is-docker')
+const MongoDB = require('fastify-mongodb')
+
+module.exports = fp(
+  async (fastify, opts) => {
+    let mongoOpts = Object.assign(
+      {},
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        url:`mongodb://${ isDocker() ? 'mongodb-web' : 'localhost'}:27017/wetalk`,
+      }, 
+      opts.mongodb
+    )
+    fastify.register(MongoDB, mongoOpts)
+  }
+)
+
+/*
+Anche in questo caso utilizziamo un plugin sviluppato dal core team di fastify, fastify-mongodb , 
+che decora il nostro microservizio aggiungendogli la possibilità di connettersi ad un istanza MongoDB.
+*/

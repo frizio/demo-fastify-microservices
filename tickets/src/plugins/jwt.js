734396c265e0b14b4827fdfd5044d56233fe75c3
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-03-27 12:17:51 +0100

    Copy plugins jwt and mongo.db

---
+++ tickets/src/plugins/jwt.js
@@ -0,0 +1,15 @@
+const fp = require(‘fastify-plugin’)
+const JWT = require(‘fastify-jwt’)
+
+module.exports = fp( 
+  async (fastify, opts) => {
+    let jwtOpts = Object.assign(
+      {}, 
+      {
+        secret: process.env.JWT_SECRET || ‘Th1s1sY0ur53cr3t’
+      }, 
+      opts.jwt
+    )
+    fastify.register(JWT, jwtOpts)
+  }
+)
---
+++ tickets/src/plugins/mongo.db.js
@@ -0,0 +1,18 @@
+const fp = require('fastify-plugin')
+const isDocker = require('is-docker')
+const MongoDB = require('fastify-mongodb')
+
+module.exports = fp(
+  async (fastify, opts) => {
+    let mongoOpts = Object.assign(
+      {},
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        url:`mongodb://${ isDocker() ? 'mongodb-web' : 'localhost'}:27017/wetalk`,
+      }, 
+      opts.mongodb
+    )
+    fastify.register(MongoDB, mongoOpts)
+  }
+)

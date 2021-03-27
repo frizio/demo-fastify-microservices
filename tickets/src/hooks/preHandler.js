8d6f63e6bf9a29cfcdb8fb90e5f91c390fe0c5b2
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-03-27 12:20:54 +0100

    Update prehandler hook

--- tickets/src/hooks/preHandler.js
+++ tickets/src/hooks/preHandler.js
@@ -2,15 +2,24 @@
 
 const fp = require('fastify-plugin')
 
-module.exports = fp(async (fastify, opts) => {
-  fastify.addHook('preHandler', async (request, reply) => {
-    // Notice: the next callback is not available when using async/await
-    // or returning a Promise. If you do invoke a next callback in this
-    // situation unexpected behavior may occur, e.g. duplicate invocation
-    // of handlers.
-  })
+module.exports = fp(
+  async (fastify, opts) => {
+    fastify.addHook(
+      'preHandler', 
+      async (request, reply) => {
+        return request.jwtVerify()
+      }
+    )
+  }
+)
 
-  // fastify.addHook('preHandler', (request, reply, next) => {
-  //   next()
-  // })
-})
+/*
+Questo hook controlla:
+1. Se la richiesta che stiamo effettuando al microservizio ha un token nell’header della richiesta.
+2. Se il token presente nell’header della richiesta è valido.
+Nel caso in cui questi controlli vadano a buon fine, 
+nell'handler della richiesta (la funzione che gestisce effettivamente la richiesta) 
+è possibile accedere ai dati utente attraverso request.user. 
+Siccome nella definizione della rotta abbiamo salvato nel nostro token di autenticazione le proprietà username e fullName, 
+queste saranno accessibili ed eventualmente utilizzate dai vostri handler.
+*/
